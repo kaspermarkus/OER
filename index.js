d9829dd2784371c9ecdca9156a0fef483e8a5e8a
kaspermarkus <kasper@markus.dk>
2015-02-27 20:24:36 +0100

    added translations, etc

--- index.js
+++ index.js
@@ -2,27 +2,23 @@ var express = require('express');
 var app = express();
 var request = require('request');
 var msTranslator = require('mstranslator');
+var cheerio = require("cheerio");
+var _ = require('underscore');
 
 require('./streetView.js');
 
 
-    // // Second parameter to constructor (true) indicates that
-    // // the token should be auto-generated.
-    // var client = new MsTranslator({
-    //   client_id: "your client_id"
-    //   , client_secret: "your client secret"
-    // }, true);
 
-    // var params = {
-    //   text: 'How\'s it going?'
-    //   , from: 'en'
-    //   , to: 'es'
-    // };
+// Translation related Language parameters:
+var fromLang = "en",
+    toLang = "es";
 
-    // // Don't worry about access token, it will be auto-generated if needed.
-    // client.translate(params, function(err, data) {
-    //       console.log(data);
-    // });
+//set up translator
+var client = new msTranslator({
+  client_id: "OER",
+  client_secret: "iCjexcXNhME+t9rOhdz7WribW+n8Pn0p207O7DpQMvA="
+}, true);
+
 
 app.get('/', function(req, res){
     //get the parameter for start and end locations:
@@ -32,62 +28,26 @@ app.get('/', function(req, res){
 
     // Route URL:
     var routeURL = "https://maps.googleapis.com/maps/api/directions/json?origin="+encodeURIComponent(start)+
-        "&destination="+encodeURIComponent(end)+"&key=AIzaSyCf2AEb5wOD8riXjDWtWLuFkYd5hHuvSX4";
+        "&destination="+encodeURIComponent(end)+"&mode&key=AIzaSyCf2AEb5wOD8riXjDWtWLuFkYd5hHuvSX4";
 
     console.log(routeURL);
 
     // Generate HTML
-    getHTMLContent(routeURL, function (html){
-     res.send(html);
+    getHTMLContent(routeURL, function (html, instructionStrings) {
+        // console.log(instructionStrings);
+        //translate all instructions
+        client.translateArray({texts: instructionStrings, from: fromLang, to: toLang}, function(err, data) {
+            $ = cheerio.load(html);
+            var translationEls = $(".translation");
+
+            _.each(data, function (val, index) {
+                // console.log("bla"+$(translationEls[index]).html());
+                $(translationEls[index]).html("("+val.TranslatedText+")");
+            });
+
+            res.send($.html());
+        });
     });
-
-
-    // Translation related Language parameters:
-    // var fromLang = "en",
-    //     toLang = "zh";
-
-    // console.log(JSON.stringify(req.query));
-
-    // //set up translator
-    // var client = new msTranslator({
-    //   client_id: "OER",
-    //   client_secret: "iCjexcXNhME+t9rOhdz7WribW+n8Pn0p207O7DpQMvA="
-    // }, true);
-
-    // var mytext = "Walk 500 meters north on market street";
-
-    // var params = {
-    //   text: 'How\'s it going?',
-    //   from: 'en',
-    //   to: 'es'
-    // };
-
-    // // Don't worry about access token, it will be auto-generated if needed.
-    // client.translate({text: mytext, from: fromLang, to: toLang}, function(err, data) {
-    //     console.log(data);
-
-    //     // var html = "<audio controls>"+
-    //     //     '<source src="http://tts-api.com/tts.mp3?q='+encodeURIComponent(mytext)+'" type="audio/mpeg">' +
-    //     //     '</audio>';
-    //     // res.send(html);
-    // });
-
-    // var mytext = "Walk 500 meters north on market street";
-    // var html = "<audio controls>"+
-    //     '<source src="http://tts-api.com/tts.mp3?q='+encodeURIComponent(mytext)+'" type="audio/mpeg">' +
-    //     '</audio>';
-    // res.send(html);
-
-    // // var url = "https://maps.googleapis.com/maps/api/directions/json?origin=" + start +
-    // //           "&destination=" + end + "&mode=" + method + "&key=AIzaSyCf2AEb5wOD8riXjDWtWLuFkYd5hHuvSX4";
-    // // request(url, function (error, response, body) {
-    // //     if (!error && response.statusCode == 200) {
-    // //         console.log(body) // Print the google web page.
-    // //         res.send(body);
-    // //     } else {
-    // //       console.log("Error occured");
-    // //     }
-    // });
 });
 
 app.listen(3000);
--- streetView.js
+++ streetView.js
@@ -2,6 +2,20 @@ var express = require('express');
 var app = express();
 var request = require('request');
 var _ = require('underscore');
+var string = require('string');
+var msTranslator = require('mstranslator');
+
+
+// Translation related Language parameters:
+var fromLang = "en",
+    toLang = "zh";
+
+//set up translator
+var client = new msTranslator({
+  client_id: "OER",
+  client_secret: "iCjexcXNhME+t9rOhdz7WribW+n8Pn0p207O7DpQMvA="
+}, true);
+
 
 var getCoordinates = function (url){
     var latitude;
@@ -16,33 +30,43 @@ var getCoordinates = function (url){
 
 getHTMLContent = function (url, callback){
 	var html = "";
+	var instructionStrings = [];
 	var inc = 0;
-	   request(url, function (error, response, body) {
-	        if (!error && response.statusCode == 200) {
-	            var parsedJSON = JSON.parse(body);
-				var parsedRoutes = parsedJSON.routes;
-				_.each(parsedRoutes, function(route, key){
-					_.each(route, function(value, key){
-						if (key == 'legs'){
-							_.each(value, function(leg, key){
-								_.each(leg.steps, function(step, key){
-									inc++;
-									var lat;
-									var lng;
-									html += "<h1>Step "+ inc + ": </h1>";
-									html += "<h2>" + step.html_instructions + "</h2>";
-									lat = step.end_location.lat;
-									lng = step.end_location.lng;
-									html = html + getImageElement(lat, lng);
-								});
-								//console.log(html);
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var parsedJSON = JSON.parse(body);
+            // console.log(JSON.stringify(parsedJSON, null, 2));
+			var parsedRoutes = parsedJSON.routes;
+			_.each(parsedRoutes, function(route, key){
+				_.each(route, function(value, key){
+					if (key == 'legs'){
+						_.each(value, function(leg, key){
+							_.each(leg.steps, function(step, key){
+								inc++;
+								var lat;
+								var lng;
+								html += "<h1>Step "+ inc + ": </h1>";
+								html += "<h2>" + step.html_instructions;
+								// Insert audio instructions:
+								var textInstructions = string(step.html_instructions).stripTags().s;
+								instructionStrings.push(textInstructions);
+								var audioURL = "http://tts-api.com/tts.mp3?q="+encodeURIComponent(textInstructions);
+								var audio_html = '<audio controls><source src="'+audioURL+'" type="audio/mpeg"></audio>';
+								html += audio_html + "<h2>";
+								//translation:
+								html += "<div class='translation'>Translation "+inc+"</div>";
+								lat = step.end_location.lat;
+								lng = step.end_location.lng;
+								html = html + getImageElement(lat, lng);
 							});
-						}
-					});
+							//console.log(html);
+						});
+					}
 				});
-			callback(html);
-	    	}//IF
-		});
+			});
+			callback(html, instructionStrings);
+    	}//IF
+	});
 }
 
 var getImageElement = function(lat, lng){
@@ -51,3 +75,40 @@ var getImageElement = function(lat, lng){
 	imageElement = imageElement + url + "'></h3>"
 	return imageElement;
 }
+
+
+
+    // var mytext = "Walk 500 meters north on market street";
+
+    // var params = {
+    //   text: 'How\'s it going?',
+    //   from: 'en',
+    //   to: 'es'
+    // };
+
+    // // Don't worry about access token, it will be auto-generated if needed.
+    // client.translate({text: mytext, from: fromLang, to: toLang}, function(err, data) {
+    //     console.log(data);
+
+    //     // var html = "<audio controls>"+
+    //     //     '<source src="http://tts-api.com/tts.mp3?q='+encodeURIComponent(mytext)+'" type="audio/mpeg">' +
+    //     //     '</audio>';
+    //     // res.send(html);
+    // });
+
+    // var mytext = "Walk 500 meters north on market street";
+    // var html = "<audio controls>"+
+    //     '<source src="http://tts-api.com/tts.mp3?q='+encodeURIComponent(mytext)+'" type="audio/mpeg">' +
+    //     '</audio>';
+    // res.send(html);
+
+    // // var url = "https://maps.googleapis.com/maps/api/directions/json?origin=" + start +
+    // //           "&destination=" + end + "&mode=" + method + "&key=AIzaSyCf2AEb5wOD8riXjDWtWLuFkYd5hHuvSX4";
+    // // request(url, function (error, response, body) {
+    // //     if (!error && response.statusCode == 200) {
+    // //         console.log(body) // Print the google web page.
+    // //         res.send(body);
+    // //     } else {
+    // //       console.log("Error occured");
+    // //     }
+    // });
